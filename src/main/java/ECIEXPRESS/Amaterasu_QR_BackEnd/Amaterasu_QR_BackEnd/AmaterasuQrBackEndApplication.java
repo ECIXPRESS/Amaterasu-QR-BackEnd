46553f8c8a8f1a405860baaaa7f4303d5823c10f
tomasote572 <tomas.ramirez@mail.escuelaing.edu.co>
2025-11-13 17:33:41 -0500

    Repositorio con tecnologias

---
+++ src/main/java/ECIEXPRESS/Amaterasu_QR_BackEnd/Amaterasu_QR_BackEnd/AmaterasuQrBackEndApplication.java
@@ -0,0 +1,13 @@
+package ECIEXPRESS.Amaterasu_QR_BackEnd.Amaterasu_QR_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmaterasuQrBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmaterasuQrBackEndApplication.class, args);
+	}
+
+}
