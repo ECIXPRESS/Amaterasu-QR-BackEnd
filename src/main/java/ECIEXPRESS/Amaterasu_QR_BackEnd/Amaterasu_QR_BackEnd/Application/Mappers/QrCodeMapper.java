c6e5359a78707782773cfad30af2e679e13b6b25
komi_yes <danielpalacios04@gmail.com>
2025-12-05 13:59:07 -0500

    refactor(mapper): Update mappers to be final and private constructor
    Refactor QrCodeMapper and ReceiptProviderMapper by marking them as final
    and adding private constructors to prevent instantiation.
    This ensures utility class behavior.

--- src/main/java/ECIEXPRESS/Amaterasu_QR_BackEnd/Amaterasu_QR_BackEnd/Application/Mappers/QrCodeMapper.java
+++ src/main/java/ECIEXPRESS/Amaterasu_QR_BackEnd/Amaterasu_QR_BackEnd/Application/Mappers/QrCodeMapper.java
@@ -2,8 +2,9 @@ package ECIEXPRESS.Amaterasu_QR_BackEnd.Amaterasu_QR_BackEnd.Application.Mappers
 
 import ECIEXPRESS.Amaterasu_QR_BackEnd.Amaterasu_QR_BackEnd.Infrastructure.Web.Dto.QrResponses.CreateQrCodeResponse;
 
-
-public class QrCodeMapper {
+public final class QrCodeMapper {
+    private QrCodeMapper() {
+    }
     public static CreateQrCodeResponse QrStringToCreateQrCodeResponse(String qrCode) {
         return new CreateQrCodeResponse(qrCode);
     }
--- src/main/java/ECIEXPRESS/Amaterasu_QR_BackEnd/Amaterasu_QR_BackEnd/Infrastructure/Clients/Receipt/Mappers/ReceiptProviderMapper.java
+++ src/main/java/ECIEXPRESS/Amaterasu_QR_BackEnd/Amaterasu_QR_BackEnd/Infrastructure/Clients/Receipt/Mappers/ReceiptProviderMapper.java
@@ -3,11 +3,13 @@ package ECIEXPRESS.Amaterasu_QR_BackEnd.Amaterasu_QR_BackEnd.Infrastructure.Clie
 import ECIEXPRESS.Amaterasu_QR_BackEnd.Amaterasu_QR_BackEnd.Infrastructure.Clients.Receipt.Dto.ReceiptRequests.GetQrReceiptRequest;
 import ECIEXPRESS.Amaterasu_QR_BackEnd.Amaterasu_QR_BackEnd.Infrastructure.Clients.Receipt.Dto.ReceiptResponses.GetQrReceiptResponse;
 
-public class ReceiptProviderMapper {
+public final class ReceiptProviderMapper {
+    private ReceiptProviderMapper() {
+    }
     public static String mapQrResponseToString(GetQrReceiptResponse response){
         return response.QRCode();
     }
-    public static GetQrReceiptResponse mapQrResponseToString(String qrCode){
+    public static GetQrReceiptResponse mapStringToQrResponse(String qrCode){
         return new GetQrReceiptResponse(qrCode);
     }
     public static GetQrReceiptRequest mapToGetQrReceiptRequest(String orderId){
